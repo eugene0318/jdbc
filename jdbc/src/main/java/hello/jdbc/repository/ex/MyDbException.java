66193a4bd221508685d3e39b95a052fb8df81398
eugene0318 <nttott@gmail.com>
2023-11-16 06:09:01 +0900

    ~데이터 접근 예외 직접 만들기

---
+++ jdbc/src/main/java/hello/jdbc/repository/ex/MyDbException.java
@@ -0,0 +1,25 @@
+package hello.jdbc.repository.ex;
+
+public class MyDbException extends RuntimeException {
+
+	public MyDbException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyDbException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyDbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyDbException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ jdbc/src/main/java/hello/jdbc/repository/ex/MyDuplicateKeyException.java
@@ -0,0 +1,25 @@
+package hello.jdbc.repository.ex;
+
+public class MyDuplicateKeyException extends MyDbException {
+
+	public MyDuplicateKeyException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyDuplicateKeyException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyDuplicateKeyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyDuplicateKeyException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
